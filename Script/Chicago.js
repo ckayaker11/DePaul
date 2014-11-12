eea38c8378fe9d924dff1a3982dc45c10c8a45b8
ckayaker11 <ckayaker11@gmail.com>
2014-11-11 21:54:00 -0600

    Initial HTML5 Homework

---
+++ Script/Chicago.js
@@ -0,0 +1,21 @@
+
+	function changeBackground(from, to) {
+		$('#mainBackground').removeClass('background').addClass('background2');	
+		//$('#mainBackground').animate({
+		//	opacity: 0.01,
+		//	height: "toggle"
+		// }, 1, function() {
+			// Animation complete.
+		//  });
+		
+		//$('#mainBackground').animate({
+		//	opacity: 1,		
+		//	height: "toggle"
+		// }, 2000, function() {
+		//	// Animation complete.
+		//  });  
+	  
+	}
+	
+ 
+ 
